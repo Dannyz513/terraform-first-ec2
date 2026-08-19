f83ddb4fb4f0ac012ccf54b3da6087b03426cb1d
Daniel Zamudio <danielzamudioi@yahoo.com>
2026-08-19 10:20:04 -0700

    First Terraform project: EC2 instance with variables and outputs

---
+++ Main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "my-first-terraform-instance"
+  }
+}
